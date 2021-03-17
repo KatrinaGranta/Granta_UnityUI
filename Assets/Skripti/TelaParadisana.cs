e8a590861e0f5b7f10403290be7f24957d6a5b9f
Katrina <katrinagranta30@gmail.com>
2021-03-17 21:31:50 +0200

    Es aizmirsu ka ir jacommito, es nezin

---
+++ Assets/Skripti/TelaParadisana.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TelaParadisana : MonoBehaviour
+{
+  
+    public GameObject mainigaisAttels;
+    public Sprite[] atteluMasivs;
+    public GameObject slaideris;
+    public GameObject slaideris2;
+
+    public void izkritosais(int skaitlis)
+    {
+        if (skaitlis == 0)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[0];
+        }
+        if (skaitlis == 1)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[1];
+        }
+        if (skaitlis == 2)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[2];
+        }
+        if (skaitlis == 3)
+        {
+            mainigaisAttels.GetComponent<Image>().sprite = atteluMasivs[3];
+        }
+    }
+    public void mainitIzmerus() {
+        float pasreizejaVertiba = slaideris.GetComponent<Slider>().value;
+        mainigaisAttels.transform.localScale = new Vector3(1F * pasreizejaVertiba, 1F * pasreizejaVertiba, 1);
+    }
+    public void mainitIzmerus2()
+    {
+        float pasreizejaVertiba = slaideris.GetComponent<Slider>().value;
+        mainigaisAttels.transform.localScale = new Vector3(1F * pasreizejaVertiba, 1F * pasreizejaVertiba, 1);
+    }
+}
+        
+    
---
+++ Assets/Skripti/VardaVecumaParadisana.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class VardaVecumaParadisana : MonoBehaviour{
+ 
+    public string teksts, teksts1;
+    
+    public GameObject Vards;
+    public GameObject Vecums;
+
+    public GameObject TekstaAttelosana;
+
+    
+    public void uzglabatTekstu(){
+        teksts = Vards.GetComponent<Text>().text;
+        teksts1 = Vecums.GetComponent<Text>().text;
+        TekstaAttelosana.GetComponent<Text>().text = "Multeņu varonis " + teksts.ToUpper() + " ir "+teksts1.ToUpper() + " gadus vecs!";
+    }
+
+}
