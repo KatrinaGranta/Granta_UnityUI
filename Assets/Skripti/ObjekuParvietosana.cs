dbd74c7de3c25e6d66b3892e5c3872f72e78888d
Katrina <katrinagranta30@gmail.com>
2021-03-18 19:30:00 +0200

    Pievienoju Drag and drop

---
+++ Assets/Skripti/ObjekuParvietosana.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ObjekuParvietosana : MonoBehaviour, IPointerDownHandler, IDragHandler, IBeginDragHandler, IEndDragHandler {
+    private RectTransform transformacijuLogs;
+    public Canvas kanva;
+
+    private void Awake()  {
+        transformacijuLogs = GetComponent<RectTransform>();
+    }
+    public void OnPointerDown(PointerEventData notikums) {
+        Debug.Log("Kreisas klikšķis uz pārvietojamā objekta!");
+    }
+    public void OnDrag(PointerEventData notikums){
+        Debug.Log("Objekts tiek pārvietots!");
+        transformacijuLogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+    }
+    public void OnBeginDrag(PointerEventData notikums){
+        Debug.Log("Uzsākta objekta pārvietošana!");
+    }
+    public void OnEndDrag(PointerEventData notikums)  {
+        Debug.Log("Pabeigta objekta pārvietošana");
+    }
+}
---
+++ Assets/Skripti/TeletubijuSkana.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TeletubijuSkana : MonoBehaviour{
+
+    public  AudioSource skanasAvots;
+    public  AudioClip SkanaKOAtskano;
+
+    public void uzbiditsUzAttela()
+    {
+        skanasAvots.PlayOneShot(SkanaKOAtskano);
+    }
+    public void kursorsNoietNoAttela(){
+        skanasAvots.Stop();
+    }
+}
